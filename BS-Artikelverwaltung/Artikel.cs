64600552488fd05ae6fe88b0ffa5628880f07ccd
MaximilianGrieser <61865913+MaximilianGrieser@users.noreply.github.com>
2022-07-04 10:42:48 +0200

    Added Classes from previous Project

---
+++ BS-Artikelverwaltung/Artikel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassen_und_OOP {
+    class Artikel {
+        public Artikel(int id, int gewicht, int bestand, string bezeichnung, double preis) {
+            this.id = id;
+            this.gewicht = gewicht;
+            this.bestand = bestand;
+            this.bezeichnung = bezeichnung;
+            this.preis = preis;
+        }
+
+        public int id { get; set; }
+        public int gewicht { get; set; }
+        public int bestand { get; set; }
+        public string bezeichnung { get; set; }
+        public double preis { get; set; }
+
+        public double getLagerwert() {
+            return this.preis * this.bestand;
+        }
+    }
+}
